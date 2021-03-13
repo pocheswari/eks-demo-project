8c323ba16d94919b0e5ed13b2aa3cccd18dc1ea4
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-03-13 13:24:43 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -75,13 +75,3 @@ resource "aws_eks_cluster" "demo" {
     aws_iam_role_policy_attachment.demo-cluster-AmazonEKSVPCResourceController,
   ]
 }
-
-terraform {
-  backend "consul" {
-    address  = "localhost:8500"
-    scheme   = "http"
-    path     = "tf/terraform.tfstate"
-    lock     = true
-    gzip     = false
-  }
-}
