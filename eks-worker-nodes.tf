218ca791074610c22b3f83acbafeb88e4b9237df
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-03-22 16:36:49 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -1,9 +1,3 @@
-#
-# EKS Worker Nodes Resources
-#  * IAM role allowing Kubernetes actions to access other AWS services
-#  * EKS Node Group to launch worker nodes
-#
-
 resource "aws_iam_role" "demo-node" {
   name = "terraform-eks-demo-node"
 
