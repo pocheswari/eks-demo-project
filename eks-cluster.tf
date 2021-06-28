c78997901f36f4084d4f00feb44fd11abac5d6fb
kodekolli <79526430+kodekolli@users.noreply.github.com>
2021-06-28 19:34:08 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "demo-cluster" {
 resource "aws_eks_cluster" "demo" {
   name     = var.cluster-name
   role_arn = aws_iam_role.demo-cluster.arn
-  version = 1.19
+  version = 1.20
 
   vpc_config {
     security_group_ids = [aws_security_group.demo-cluster.id]
